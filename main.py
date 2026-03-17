481de9388a221a93c15ddf2dc7bc7f5c9cc62da8
santiago200508 <kevin.santiago8580@gmail.com>
2026-03-16 20:20:43 -0500

    Redirect raiz a productos

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from metodos import rutas
 
 app = FastAPI()
 
 app.include_router(rutas.router)
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/productos")
